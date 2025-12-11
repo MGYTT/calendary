a14711d84602c2ca27df3dccbcf6829c4b107525
MGYTT <139177952+MGYTT@users.noreply.github.com>
2025-12-11 10:13:44 +0100

    Remove ThemeProvider component implementation

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,11 +1,1 @@
-'use client';
 
-import React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-
-// Typ wyciągamy z samego komponentu, bez /dist/types
-type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
